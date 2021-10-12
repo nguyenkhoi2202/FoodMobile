836db75afab787c07876463087238e69d27ddd0d
khoitn-fpt <63914061+khoitn-fpt@users.noreply.github.com>
2021-10-12 08:22:15 +0700

    create app

---
+++ app/src/main/java/com/example/foodmobile/FoodMenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.foodmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_menu);
+
+    }
+}
