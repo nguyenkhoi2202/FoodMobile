cbaa55ffb0ddf66efcce83c5c09d4172bf078206
khoitn-fpt <63914061+khoitn-fpt@users.noreply.github.com>
2021-10-12 08:24:08 +0700

    create branch khoitn

--- app/src/main/java/com/example/foodmobile/FoodMenuActivity.java
+++ app/src/main/java/com/example/foodmobile/FoodMenuActivity.java
@@ -10,6 +10,6 @@ public class FoodMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_menu);
-
+        System.out.println("create app");
     }
 }
